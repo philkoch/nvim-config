8516a4d3e5e5d3717a7f5d7dd2735bbc32078fbd
Philip Koch <philipkoch@live.de>
2023-02-19 23:22:22 +0100

    played around with trouble trying to fix signs in signcolumn

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,14 +1,34 @@
 return {
-		"folke/trouble.nvim",
-		dependencies = "kyazdani42/nvim-web-devicons",
-		config = function()
-			require("trouble").setup({})
-			vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", {silent = true, noremap = true})
-			vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", {silent = true, noremap = true})
-			vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", {silent = true, noremap = true})
-			vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", {silent = true, noremap = true})
-			vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", {silent = true, noremap = true})
-			vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", {silent = true, noremap = true})
-			vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", {silent = true, noremap = true})
-		end,
-	}
+	"folke/trouble.nvim",
+	dependencies = "kyazdani42/nvim-web-devicons",
+	config = function()
+		require("trouble").setup({
+			signs = {
+				-- icons / text used for a diagnostic
+				error = "",
+				warning = "",
+				hint = "",
+				information = "",
+				other = "﫠",
+			},
+			use_diagnostic_signs = false, -- enabling this will use the signs defined in your lsp client
+		})
+		vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", { silent = true, noremap = true })
+		vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", { silent = true, noremap = true })
+		vim.keymap.set(
+			"n",
+			"<leader>xw",
+			"<cmd>TroubleToggle workspace_diagnostics<cr>",
+			{ silent = true, noremap = true }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>xd",
+			"<cmd>TroubleToggle document_diagnostics<cr>",
+			{ silent = true, noremap = true }
+		)
+		vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", { silent = true, noremap = true })
+		vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", { silent = true, noremap = true })
+		vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", { silent = true, noremap = true })
+	end,
+}
