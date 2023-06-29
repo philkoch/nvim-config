4b120b76e0e4852dd7a416a3d751949b0a48be64
Philip Koch <philipkoch@live.de>
2023-06-29 14:19:05 +0200

    added ruff in null-ls config

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -22,8 +22,9 @@ return {
                 }),
                 null_ls.builtins.formatting.isort,
                 -- null_ls.builtins.diagnostics.pyproject_flake8,
-                -- null_ls.builtins.diagnostics.mypy,
+                null_ls.builtins.diagnostics.mypy,
                 null_ls.builtins.diagnostics.ruff,
+                null_ls.builtins.formatting.ruff,
                 -- null_ls.builtins.diagnostics.pylint,
                 -- null_ls.builtins.diagnostics.pydocstyle,
 
