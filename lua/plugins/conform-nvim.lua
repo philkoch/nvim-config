bc92537b733d10063bc918666af69fd8a0416322
Philip Koch <philipkoch@live.de>
2023-10-13 11:59:38 +0200

    added formatter for xml in conform

--- lua/plugins/conform-nvim.lua
+++ lua/plugins/conform-nvim.lua
@@ -8,6 +8,12 @@ return {
 				lua = { "stylua" },
 				python = { "isort", "black" },
 				javascript = { { "prettierd", "prettier" } },
+				xml = { "xmlformat" },
+			},
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 500,
+				lsp_fallback = true,
 			},
 		})
 
