df7e2b19008b11a113fa3ce1758656e5e4c09d6e
Philip Koch <philipkoch@live.de>
2023-11-14 09:30:09 +0100

    updated indent-blankline config to work with v3

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -2,17 +2,10 @@ return {
 	"lukas-reineke/indent-blankline.nvim",
 	name = "indent-blankline",
 	lazy = false,
-	config = function()
-		require("indent_blankline").setup({
-			space_char_blankline = " ",
-			char_highlight_list = {
-				"IndentBlanklineIndent1",
-				"IndentBlanklineIndent2",
-				"IndentBlanklineIndent3",
-				"IndentBlanklineIndent4",
-				"IndentBlanklineIndent5",
-				"IndentBlanklineIndent6",
-			},
-		})
-	end,
+	main = "ibl",
+	opts = {
+		indent = {
+			highlight = { "Function", "Label" },
+		},
+	},
 }
